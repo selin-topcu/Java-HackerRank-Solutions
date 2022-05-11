d7c249487c4e54e206f2a8789d7c4277b1028fe5
Selin Dayıoğlu <slndayioglu@gmail.com>
2022-05-11 13:30:33 +0300

    Time Conversion,Birhday Solutions Added

---
+++ ProblemSolving-Java/10-Time Conversion.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'timeConversion' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String timeConversion(String s) {
+    // Write your code here
+     if(s.charAt(8)=='P'){
+
+        String portion = s.substring(0,8);
+        String[] times = portion.split(":");
+        if(times[0].matches("12")){
+            return times[0] + ":" + times[1] + ":" + times[2];
+        }
+        times[0] = String.valueOf(Integer.valueOf(times[0]) + 12);
+        return times[0] + ":" + times[1] + ":" + times[2];
+     }else{
+         String portion = s.substring(0,8);
+        String[] times = portion.split(":");
+        if(times[0].matches("12")){
+            times[0]= "00";
+            return times[0] + ":" + times[1] + ":" + times[2];
+        }
+         return s.substring(0,8);
+     }
+    
+    
+    
+    }}
